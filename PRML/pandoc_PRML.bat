5fed49d104402fe528f16295db7649e60a42ccd3
zYx.Tom <zhuyuanxiang@gmail.com>
2020-05-03 18:06:57 +0800

    更新了 元数据 后的 批处理文件
    Signed-off-by: zYx.Tom <zhuyuanxiang@gmail.com>

--- PRML/pandoc_PRML.bat
+++ PRML/pandoc_PRML.bat
@@ -1,2 +1,9 @@
-pandoc PRML/C_01.md PRML/reference.md -o tmp/PRML_tmp.md --atx-headers --wrap=preserve
-REM pandoc PRML/C_01.md  -o tmp/PRML_tmp.md -r markdown+footnotes+all_symbols_escapable --atx-headers --wrap=preserve
+REM pandoc PRML/preface.md PRML/C_01.md PRML/C_02.md PRML/C_03.md -o tmp/PRML_tmp.pdf --log=tmp/PRML_tmp.log --template=template/zYxTom.tex --pdf-engine=xelatex -V CJKmainfont="Microsoft YaHei" -V mainfont="Arial Unicode MS" -V monofont="Cambria Math"
+
+REM pandoc -d prml/prml-tex.yaml
+REM Code tmp/prml_tmp.tex
+
+REM pandoc PRML/C_04.md -o tmp/PRML_tmp.tex --log=tmp/PRML_tmp.log --template=template/zYxTom.tex --pdf-engine=xelatex -V CJKmainfont="Microsoft YaHei" -V mainfont="Arial Unicode MS" -V monofont="Cambria Math"
+
+pandoc -d prml/prml-pdf.yaml
+sumatrapdf tmp/prml_tmp.pdf
