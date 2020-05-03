de70f93f7bdfdacd625c377082225dd0586f7775
zYx.Tom <zhuyuanxiang@gmail.com>
2020-05-03 18:06:21 +0800

    更新了 元数据 文件后的批处理文件
    Signed-off-by: zYx.Tom <zhuyuanxiang@gmail.com>

--- ML/pandoc_ml.bat
+++ ML/pandoc_ml.bat
@@ -1,11 +1,16 @@
-REM -------------------------------------------------------
-cls
-REM pandoc ML/metadata.yaml ML/Outline.md ML/Regression.md -o tmp/ML_PPT.pptx --reference-doc=template/reference.pptx
-
-pandoc ML/Regression.md -o tmp/ML_PPT.pptx --reference-doc=template/reference.pptx
-
+REM pandoc ML/metadata.yaml ML/Outline.md ML/Regression.md ML/reference.md -o tmp/ML_PPT.pptx --reference-doc=template/reference.pptx
+pandoc ML/Regression.md ML/reference.md -o tmp/ML_PPT.pptx --reference-doc=template/mathematics.pptx
 powerpnt tmp/ML_PPT.pptx
+
 REM -------------------------------------------------------
-cls
 REM pandoc tmp/sample.md -o tmp/sample.pptx --reference-doc=template/reference.pptx
-REM powerpnt pandoc/sample.pptx
+REM powerpnt tmp/sample.pptx
+
+REM -------------------------------------------------------
+REM pandoc tmp/test.md -o tmp/test.pptx --reference-doc=template/reference.pptx
+REM powerpnt tmp/test.pptx
+
+REM -------------------------------------------------------
+REM pandoc ML/metadata.yaml ML/Outline.md ML/reference.md -t beamer -o tmp/ML_PPT.pdf --template=template/beamer-template.tex --pdf-engine=xelatex -V CJKmainfont="SimSun" -V mainfont="Arial"
+REM sumatrapdf tmp/ML_PPT.pdf
+REM TeXworks tmp/ML_PPT.tex
