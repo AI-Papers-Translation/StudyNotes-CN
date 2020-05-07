7f7c740b7666371b5b3db94cd6e057e768573765
zYx.Tom <zhuyuanxiang@gmail.com>
2020-05-07 18:41:11 +0800

    增加了提醒声音，使用 YAML 进行 编译
    Signed-off-by: zYx.Tom <zhuyuanxiang@gmail.com>

--- PRML/pandoc_PRML.bat
+++ PRML/pandoc_PRML.bat
@@ -1,9 +1,12 @@
 REM pandoc PRML/preface.md PRML/C_01.md PRML/C_02.md PRML/C_03.md -o tmp/PRML_tmp.pdf --log=tmp/PRML_tmp.log --template=template/zYxTom.tex --pdf-engine=xelatex -V CJKmainfont="Microsoft YaHei" -V mainfont="Arial Unicode MS" -V monofont="Cambria Math"
+REM pandoc PRML/C_03.md -o tmp/PRML_tmp.tex --log=tmp/PRML_tmp.log --template=template/zYxTom.tex --pdf-engine=xelatex -V CJKmainfont="Microsoft YaHei" -V mainfont="Arial Unicode MS" -V monofont="Cambria Math" --reference-location="document"
 
 REM pandoc -d prml/prml-tex.yaml
-REM Code tmp/prml_tmp.tex
+pandoc --defaults=prml/prml-pdf.yaml
 
-REM pandoc PRML/C_04.md -o tmp/PRML_tmp.tex --log=tmp/PRML_tmp.log --template=template/zYxTom.tex --pdf-engine=xelatex -V CJKmainfont="Microsoft YaHei" -V mainfont="Arial Unicode MS" -V monofont="Cambria Math"
 
-pandoc -d prml/prml-pdf.yaml
-sumatrapdf tmp/prml_tmp.pdf
+
+REM 增加了结束前的提醒声音
+ECHO **
+REM Code tmp/PRML_tmp.tex
+sumatrapdf prml/prml-notes.pdf
